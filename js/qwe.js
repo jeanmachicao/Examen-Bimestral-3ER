e853361c520f67ee4a927b8f57ff7c3deed69b8c
Jean Percy Machicao Ruelas <jeanpercy123456789@gmail.com>
2021-10-05 09:51:59 -0500

    first commit

---
+++ js/qwe.js
@@ -0,0 +1,41 @@
+const btn_a = document.getElementById("btn_a")
+const btn_s = document.getElementById("btn_s")
+const btn_d = document.getElementById("btn_d")
+const btn_f = document.getElementById("btn_f")
+const btn_g = document.getElementById("btn_g")
+const btn_h = document.getElementById("btn_h")
+const btn_j = document.getElementById("btn_j")
+const btn_k = document.getElementById("btn_k")
+const audio_a = document.getElementById("audio_a")
+const audio_s = document.getElementById("audio_s")
+const audio_d = document.getElementById("audio_d")
+const audio_f = document.getElementById("audio_f")
+const audio_g = document.getElementById("audio_g")
+const audio_h = document.getElementById("audio_h")
+const audio_j = document.getElementById("audio_j")
+const audio_k = document.getElementById("audio_k")
+
+btn_a.addEventListener('click',()=>{
+    audio_a.play();
+})
+btn_s.addEventListener('click',()=>{
+    audio_s.play();
+})
+btn_d.addEventListener('click',()=>{
+    audio_d.play();
+})
+btn_f.addEventListener('click',()=>{
+    audio_f.play();
+})
+btn_g.addEventListener('click',()=>{
+    audio_g.play();
+})
+btn_h.addEventListener('click',()=>{
+    audio_h.play();
+})
+btn_j.addEventListener('click',()=>{
+    audio_j.play();
+})
+btn_k.addEventListener('click',()=>{
+    audio_k.play();
+})
